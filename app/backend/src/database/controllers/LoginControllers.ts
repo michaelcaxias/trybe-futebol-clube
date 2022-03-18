f0ea08d6129b5b71a4f63b9562e1d9b68b9aaac6
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-18 17:18:48 -0300

    feat: ajusta loginController

--- app/backend/src/database/controllers/LoginControllers.ts
+++ app/backend/src/database/controllers/LoginControllers.ts
@@ -1,17 +1,13 @@
 import { Request, Response } from 'express';
+import * as UserServices from '../services/UserServices';
 
 export const reoveLintWarn = '';
 
 export const checkLogin = async (req: Request, res: Response) => {
-  const { email } = req.body;
-  const data = {
-    user: {
-      id: 1,
-      username: 'Admin',
-      role: 'admin',
-      email,
-    },
-    token: '123.456.789',
-  };
+  const { email, password } = req.body;
+  const { status, message, data } = await UserServices.getUserById({ email, password });
+  if (status >= 400) {
+    return res.status(status).json({ message });
+  }
   return res.status(200).json(data);
 };
