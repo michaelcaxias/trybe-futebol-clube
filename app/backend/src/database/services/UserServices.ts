410d9af89eb1d1663b7150da3584c8e8c7983634
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-18 20:59:02 -0300

    feat: implementa findOne

--- app/backend/src/database/services/UserServices.ts
+++ app/backend/src/database/services/UserServices.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs';
+import * as jwt from 'jsonwebtoken';
+
 import Users from '../models/Users';
 import IResValidate from '../interfaces/IResponseValidate';
 import responseValidate from '../utils';
@@ -7,6 +10,18 @@ type UserBody = { email: string, password: string };
 export const removeLint = '';
 
 export const getUserById = async ({ email, password }: UserBody): Promise<IResValidate> => {
-  const findUser = await Users.findOne({ where: { email, password } }) || {};
-  return responseValidate(200, '', findUser);
+  const user = await Users.findOne({ where: { email, password } }) || {};
+
+  if (!user) {
+    return responseValidate(401, 'Incorrect email or password');
+  }
+
+  const jwtSecret = fs.readFileSync('jwt.evaluation.key', 'utf8');
+  const token = jwt.sign(user, jwtSecret);
+
+  const responseFormat = {
+    user: { ...user }, token,
+  };
+
+  return responseValidate(200, '', responseFormat);
 };
