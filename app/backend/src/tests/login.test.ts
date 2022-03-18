9bda082557e4a7ac7091ea1d59f0e5e2fa57eed3
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-18 14:25:54 -0300

    feat: cobre 20% da pasta src

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -10,12 +10,7 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Verifica MSC de Login', () => {
-
-  const payload = {
-    email: "admin@admin.com",
-    password: "senha",
-  }
+describe('Verifica rota /login', () => {
 
   const response = {
     user: {
@@ -26,10 +21,39 @@ describe('Verifica MSC de Login', () => {
     },
     token: "123.456.789"
   }
-
+  
   let chaiHttpResponse: Response;
+  
+  describe('Verifica funcionamento do método POST em casos de sucesso', () => {
 
-  it('Essa requisição deve retornar código de status 200', () => {
-    expect(response).to.have.property('user');
-  });
+    const payload = {
+      email: "admin@admin.com",
+      password: "senha",
+    }
+
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).post('/login').send(payload);
+    })
+  
+    it('Retorna os dados esperados ao fazer uma requisição correta', () => {
+      expect(chaiHttpResponse.body).to.be.equal(response);
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+  })
+
+  describe('Verifica funcionamento do método POST em casos de falha', () => {
+    const payload = {
+      "email": "admin@admin.com",
+      "password": "senha",
+    }
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).post('/login').send(payload);
+    })
+  
+    it('Retorna os dados esperados ao fazer uma requisição incorreta', () => {
+      // https://stackoverflow.com/questions/38497731/mocha-chai-uncaught-assertionerror-expected-to-equal-expected-actua
+      expect(chaiHttpResponse.body).to.deep.equal({});
+      expect(chaiHttpResponse.status).to.be.equal(404);
+    });
+  })
 });
