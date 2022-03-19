764e72c0b1188fe5c59b9eebc8f7914ae6bc34fa
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 20:44:48 -0300

    feat: adiciona mock correto para clubs

--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -1,32 +1,84 @@
 export const userFindOneMock = {
   id: 1,
-  username: 'Admin',
-  role: 'admin',
-  password: '123456',
-  email: 'admin@admin.com'
-}
+  username: "Admin",
+  role: "admin",
+  password: "123456",
+  email: "admin@admin.com",
+};
 
 export const mockResponseLogin = {
   user: {
     id: 1,
     username: "Admin",
     role: "admin",
-    email: "admin@admin.com"
+    email: "admin@admin.com",
   },
-  token: "123.456.789"
-}
+  token: "123.456.789",
+};
 
 export const clubsGetMock = [
   {
-		id: 1,
-		clubName: "Avaí/Kindermann"
-	},
-	{
-		id: 2,
-		clubName: "Bahia"
-	},
-	{
-		id: 3,
-		clubName: "Botafogo"
-	}
-]
+    id: 1,
+    clubName: "Avaí/Kindermann",
+  },
+  {
+    id: 2,
+    clubName: "Bahia",
+  },
+  {
+    id: 3,
+    clubName: "Botafogo",
+  },
+  {
+    id: 4,
+    clubName: "Corinthians",
+  },
+  {
+    id: 5,
+    clubName: "Cruzeiro",
+  },
+  {
+    id: 6,
+    clubName: "Ferroviária",
+  },
+  {
+    id: 7,
+    clubName: "Flamengo",
+  },
+  {
+    id: 8,
+    clubName: "Grêmio",
+  },
+  {
+    id: 9,
+    clubName: "Internacional",
+  },
+  {
+    id: 10,
+    clubName: "Minas Brasília",
+  },
+  {
+    id: 11,
+    clubName: "Napoli-SC",
+  },
+  {
+    id: 12,
+    clubName: "Palmeiras",
+  },
+  {
+    id: 13,
+    clubName: "Real Brasília",
+  },
+  {
+    id: 14,
+    clubName: "Santos",
+  },
+  {
+    id: 15,
+    clubName: "São José-SP",
+  },
+  {
+    id: 16,
+    clubName: "São Paulo",
+  },
+];
