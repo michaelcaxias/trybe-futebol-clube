00fee117111e67bf6a59e207c2260131502be079
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 19:41:44 -0300

    fix: ajusta descrição incorreta

--- app/backend/src/tests/server.test.ts
+++ app/backend/src/tests/server.test.ts
@@ -4,7 +4,7 @@ import PORT from '../server';
 
 const { expect } = chai;
 
-describe('Teste uso do arquivo server.ts', () => {
+describe('Testa uso do arquivo server.ts', () => {
   it('Verifica se a porta está correta', () => {
     expect(PORT).to.be.equal('3001');
   });
