361bcb1e1942ca31604e5038bf9c62f7272e383c
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 16:22:38 -0300

    feat: corrige objeto jwt e remove password

--- app/backend/src/database/services/UserServices.ts
+++ app/backend/src/database/services/UserServices.ts
@@ -9,15 +9,15 @@ type UserBody = { email: string, password: string };
 
 export const removeLint = '';
 
-export const getUserById = async ({ email, password }: UserBody): Promise<IResValidate> => {
-  const user = await Users.findOne({ where: { email, password } });
+export const getUserById = async ({ email }: UserBody): Promise<IResValidate> => {
+  const user = await Users.findOne({ where: { email } });
 
   if (!user) {
     return responseValidate(401, 'Incorrect email or password');
   }
 
-  const jwtSecret = fs.readFileSync('jwt.evaluation.key', 'utf8');
-  const token = jwt.sign(user, jwtSecret);
+  const jwtSecret = fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
+  const token = jwt.sign({ email }, jwtSecret);
 
   const responseFormat = {
     user: { ...user }, token,
