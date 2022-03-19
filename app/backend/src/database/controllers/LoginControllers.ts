405275bc9a5b89023cc6cdced2edbc216a448bb6
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 18:10:32 -0300

    feat: cria controller da rota validate

--- app/backend/src/database/controllers/LoginControllers.ts
+++ app/backend/src/database/controllers/LoginControllers.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
 import * as UserServices from '../services/UserServices';
 
-export const reoveLintWarn = '';
-
 export const checkLogin = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const { status, message, data } = await UserServices.getUserById({ email, password });
+  if (status >= 400) {
+    return res.status(status).json({ message });
+  }
+  return res.status(200).json(data);
+};
+
+export const validate = async (req: Request, res: Response) => {
+  const { authorization } = req.headers;
+  const { status, message, data } = await UserServices.getRoleByToken(authorization);
   if (status >= 400) {
     return res.status(status).json({ message });
   }
