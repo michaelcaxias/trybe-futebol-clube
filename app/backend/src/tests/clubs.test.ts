3e14377be0b8554d0c44f65e493688d9b651120a
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 20:46:06 -0300

    feat: renomeia e cria testes de integração para rota /clubs/1

--- app/backend/src/tests/clubs.test.ts
+++ app/backend/src/tests/clubs.test.ts
@@ -6,7 +6,7 @@ import { app } from '../app';
 import { Response } from 'superagent';
 
 
-import { clubsGetMock } from './mocks/';
+import { clubsGetMock } from './mocks';
 
 chai.use(chaiHttp);
 
@@ -20,6 +20,19 @@ describe('Testa uso do endpoint /clubs', () => {
     it('Retorna os dados esperados ao fazer uma requisição correta', async () => {
       chaiHttpResponse = await chai.request(app).get('/clubs');
       expect(chaiHttpResponse.body).to.be.equal(clubsGetMock);
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+  })
+});
+
+describe('Testa uso do endpoint /clubs/:id', () => {
+
+  let chaiHttpResponse: Response;
+  
+  describe('Verifica funcionamento do método GET em casos de sucesso', () => {
+    it('Retorna os dados esperados ao fazer uma requisição correta', async () => {
+      chaiHttpResponse = await chai.request(app).get('/clubs/1');
+      expect(chaiHttpResponse.body).to.be.equal(clubsGetMock[0]);
       expect(chaiHttpResponse.status).to.be.equal(200);
     });
   })
