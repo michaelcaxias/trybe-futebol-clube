1f4280ebecfdc0057bf4c7b018ef4725b2f34f40
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 20:36:22 -0300

    feat: cria getTeamById no controller

--- app/backend/src/database/controllers/ClubControllers.ts
+++ app/backend/src/database/controllers/ClubControllers.ts
@@ -10,3 +10,13 @@ export const getTeams = async (_req: Request, res: Response) => {
   }
   return res.status(200).json(data);
 };
+
+export const getTeamById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const convertIdToNumber = Number(id);
+  const { status, message, data } = await ClubServices.getTeamById(convertIdToNumber);
+  if (status >= 400) {
+    return res.status(status).json({ message });
+  }
+  return res.status(200).json(data);
+};
