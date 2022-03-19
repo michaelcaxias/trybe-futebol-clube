9375ccd04173af3826eabe8a00731a877fb2e886
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 16:10:34 -0300

    feat: adiciona mock ao findone

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -38,15 +38,22 @@ describe('Verifica rota /login', () => {
   })
 
   describe('Verifica funcionamento do método POST em casos de falha', () => {
+
     const payload = {
       email: "admin@admin.com",
       password: "senha"
     }
+
     before(async () => {
-      chaiHttpResponse = await chai.request(app).post('/login').send(payload);
+      sinon.stub(Users, "findOne").resolves(userFindOneMock as Users);
     })
-  
-    it('Retorna os dados esperados ao fazer uma requisição incorreta', () => {
+
+    after(async () => {
+      (Users.findOne as sinon.SinonStub).restore();
+    })
+    
+    it('Retorna os dados esperados ao fazer uma requisição incorreta', async () => {
+      chaiHttpResponse = await chai.request(app).post('/login').send(payload);
       // https://stackoverflow.com/questions/38497731/mocha-chai-uncaught-assertionerror-expected-to-equal-expected-actua
       expect(chaiHttpResponse.body).to.deep.equal({});
       expect(chaiHttpResponse.status).to.be.equal(404);
