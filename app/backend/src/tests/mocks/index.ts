cef21857535837f861d27afa03b4b0ecb3b53312
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 16:03:06 -0300

    feat: cria arquivo para mocks

---
+++ app/backend/src/tests/mocks/index.ts
@@ -0,0 +1,19 @@
+export const rmeoveLint = '';
+
+export const userFindOneMock = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  password: '123456',
+  email: 'admin@admin.com'
+}
+
+export const mockResponseLogin = {
+  user: {
+    id: 1,
+    username: "Admin",
+    role: "admin",
+    email: "admin@admin.com"
+  },
+  token: "123.456.789"
+}
