f32176f07764b76e5e707ae688bfa848f5d782e3
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-20 18:11:09 -0300

    feat: cria função postMatch em services

--- app/backend/src/database/services/MatchsServices.ts
+++ app/backend/src/database/services/MatchsServices.ts
@@ -1,7 +1,12 @@
+import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs';
 import responseValidate from '../utils';
 import IResValidate from '../interfaces/IResponseValidate';
 import Matchs from '../models/Matchs';
 import Clubs from '../models/Clubs';
+import IMatch from '../interfaces/IMatch';
+import Users from '../models/Users';
+import ErrorMessage from '../utils/ErrorMessage';
 
 export const getMatchs = async (): Promise<IResValidate> => {
   const matchs = await Matchs.findAll({
@@ -32,4 +37,29 @@ export const getMatchsByProgress = async (inProgress: boolean): Promise<IResVali
   }
 
   return responseValidate(200, '', matchs);
+};
+
+export const getJWTUserByToken = async (decodedJWT: string | jwt.JwtPayload) => {
+  if (typeof decodedJWT === 'object') {
+    const user = await Users.findOne({ where: { email: decodedJWT.email } });
+    return user;
+  }
+  return '';
+};
+
+export const postMatch = async (newMatch: IMatch, token: string): Promise<IResValidate> => {
+  const jwtSecret = fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
+  const verifyToken = jwt.verify(token, jwtSecret);
+  const user = await getJWTUserByToken(verifyToken);
+
+  if (!user) {
+    return responseValidate(401, ErrorMessage.INVALID_TOKEN);
+  }
+
+  const createNewGame = await Matchs.create(newMatch);
+
+  if (!createNewGame) {
+    return responseValidate(400, 'Could not create the match specified');
+  }
+  return responseValidate(200, '', createNewGame);
 };
