532f48d18ba236d03f6fd9dd008ed596ffdcfb44
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-20 18:16:17 -0300

    feat: cria função para verificar token jwt

--- app/backend/src/database/utils/index.ts
+++ app/backend/src/database/utils/index.ts
@@ -1,3 +1,5 @@
+import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs';
 import IResValidate from '../interfaces/IResponseValidate';
 import Users from '../models/Users';
 
@@ -13,4 +15,11 @@ export const getJWTUserByToken = async (decodedJWT: string | jwt.JwtPayload) => 
     return user;
   }
   return '';
+};
+
+export const verifyJWT = async (token: string) => {
+  const jwtSecret = fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
+  const verifyToken = jwt.verify(token, jwtSecret);
+  const user = await getJWTUserByToken(verifyToken);
+  return user;
 };
