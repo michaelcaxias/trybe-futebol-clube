860d0c878f60164b20a378e08c5310e6609dabf1
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 21:32:21 -0300

    feat: cria rota para matchs in progress

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -34,6 +34,7 @@ class App {
     this.app.route('/clubs').get(Club.getTeams);
     this.app.route('/clubs/:id').get(validateId, Club.getTeamById);
     this.app.route('/matchs').get(Match.getMatchs);
+    this.app.route('/matchs/:inProgress').get(Match.getMatchsByProgress);
     this.app.listen(PORT, () => {
       console.log(`Rodando na porta ${PORT}`);
     });
