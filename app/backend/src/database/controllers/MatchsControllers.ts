9c4efe7205f2fa3b5b1a7ec83e6f02b064dd63e3
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 21:32:34 -0300

    feat: cria getmachsbyprogress controller

--- app/backend/src/database/controllers/MatchsControllers.ts
+++ app/backend/src/database/controllers/MatchsControllers.ts
@@ -10,3 +10,16 @@ export const getMatchs = async (_req: Request, res: Response) => {
   }
   return res.status(200).json(data);
 };
+
+export const getMatchsByProgress = async (req: Request, res: Response) => {
+  const { inProgress } = req.params;
+  const convertInProgressToBoolean = inProgress === 'true' ? 1 : 0;
+  const {
+    status, message, data,
+  } = await MatchServices.getMatchsByProgress(convertInProgressToBoolean);
+
+  if (status >= 400) {
+    return res.status(status).json({ message });
+  }
+  return res.status(200).json(data);
+};
