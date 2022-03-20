11b669c8bf3bbad15d56598526cac2be057aa498
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-19 21:39:46 -0300

    fix: remove variavel de lint

--- app/backend/src/database/controllers/MatchsControllers.ts
+++ app/backend/src/database/controllers/MatchsControllers.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
 import * as MatchServices from '../services/MatchsServices';
 
-export const removeLint = '';
-
 export const getMatchs = async (_req: Request, res: Response) => {
   const { status, message, data } = await MatchServices.getMatchs();
   if (status >= 400) {
