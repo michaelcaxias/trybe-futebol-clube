d33ec44819a38347ca04ffa9a9ea24d86839e545
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-20 18:02:13 -0300

    feat: cria novo enum

--- app/backend/src/database/utils/ErrorMessage.ts
+++ app/backend/src/database/utils/ErrorMessage.ts
@@ -1,6 +1,7 @@
 enum ErrorMessage {
   NOT_EMPTY = 'All fields must be filled',
   INCORRECT_LOGIN = 'Incorrect email or password',
+  INVALID_TOKEN = 'Invalid Token',
 }
 
 export default ErrorMessage;
