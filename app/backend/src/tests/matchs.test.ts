a758f8edcbf2f61cfd1a2ff921d20690f681209b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-21 20:46:58 -0300

    fix: corrige erros de matchs

--- app/backend/src/tests/matchs.test.ts
+++ app/backend/src/tests/matchs.test.ts
@@ -1,6 +1,5 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
-import * as MatchsServices from '../database/services/MatchsServices';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
@@ -8,7 +7,6 @@ import { Response } from 'superagent';
 
 import Matchs from '../database/models/Matchs';
 import Users from '../database/models/Users';
-import { responseValidate } from '../database/utils/';
 
 import {
   matchsGetMock,
@@ -31,16 +29,14 @@ describe('Testa uso do endpoint /matchs', () => {
   describe('Verifica funcionamento do método GET em casos de sucesso', () => {
     before(async () => {
       sinon.stub(Matchs, "findAll").resolves(matchFindAll as Matchs[]);
-      sinon.stub(MatchsServices, "getMatchs").resolves(responseValidate(200, '', matchFindAll));
     })
 
     after((async () => {
       (Matchs.findAll as sinon.SinonStub).restore();
-      (MatchsServices.getMatchs as sinon.SinonStub).restore();
     }))
     it('Retorna os dados esperados ao fazer uma requisição correta', async () => {
       chaiHttpResponse = await chai.request(app).get('/matchs');
-      expect(chaiHttpResponse.body).to.deep.equal(matchsGetMock);
+      expect(chaiHttpResponse.body).to.deep.equal(matchFindAll);
       expect(chaiHttpResponse.body).to.be.an('array');
       expect(chaiHttpResponse.status).to.be.equal(200);
     });
@@ -50,13 +46,11 @@ describe('Testa uso do endpoint /matchs', () => {
     const authorization = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY0NzcyNTQ4N30.liM1Oa_nEGRshFcjd4gz8JWPoTHXKML-dATZVOzKb2A"
 
     before(async () => {
-      sinon.stub(MatchsServices, "postMatch").resolves(responseValidate(200, '', matchPostMock));
       sinon.stub(Users, "findOne").resolves(userFindOneMock as Users);
       sinon.stub(Matchs, "create").resolves(matchFindAll[0] as Matchs);
     })
 
     after((async () => {
-      (MatchsServices.getMatchs as sinon.SinonStub).restore();
       (Matchs.create as sinon.SinonStub).restore();
       (Users.findOne as sinon.SinonStub).restore();
     }))
@@ -70,25 +64,23 @@ describe('Testa uso do endpoint /matchs', () => {
         inProgress: false,
       }
       chaiHttpResponse = await chai.request(app).post('/matchs').send(payload).set('Authorization', authorization);
-      expect(chaiHttpResponse.body).to.deep.equal(matchPostMock);
+      expect(chaiHttpResponse.body).to.deep.equal(matchFindAll[0]);
       expect(chaiHttpResponse.status).to.be.equal(200);
     });
   })
 
   describe('Verifica funcionamento do método GET em casos de erro', () => {
     before(async () => {
-      sinon.stub(MatchsServices, "getMatchs").resolves(responseValidate(404, '', 'Could not find any Matchs'));
-      sinon.stub(Matchs, "findAll").resolves();
+      sinon.stub(Matchs, "findAll").resolves([]);
     })
 
     after((async () => {
-      (MatchsServices.getMatchs as sinon.SinonStub).restore();
       (Matchs.findAll as sinon.SinonStub).restore();
     }))
 
     it('Retorna um erro ao fazer uma requisição sem existir Matchs no DB', async () => {
       chaiHttpResponse = await chai.request(app).get('/matchs');
-      expect(chaiHttpResponse.body).to.be.equal({ message: 'Could not find any Matchs' });
+      expect(chaiHttpResponse.body).to.deep.equal({ message: 'Could not find any Matchs' });
       expect(chaiHttpResponse.status).to.be.equal(404);
     });
   })
@@ -97,19 +89,18 @@ describe('Testa uso do endpoint /matchs', () => {
     let authorization = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY0NzcyNTQ4N30.liM1Oa_nEGRshFcjd4gz8JWPoTHXKML-dATZVOzKb2A"
 
     before(async () => {
-      sinon.stub(MatchsServices, "postMatch").resolves(responseValidate(401, "Invalid Token"));
       sinon.stub(Users, "findOne").resolves(userFindOneMock as Users);
       sinon.stub(Matchs, "create").resolves();
     })
 
     after((async () => {
-      (MatchsServices.getMatchs as sinon.SinonStub).restore();
       (Matchs.create as sinon.SinonStub).restore();
       (Users.findOne as sinon.SinonStub).restore();
     }))
 
     it('Retorna uma mensagem de erro no envio de um token incorreto', async () => {
       payload = {
+        homeTeam: 16,
         awayTeam: 8,
         homeTeamGoals: 2,
         awayTeamGoals: 2,
@@ -139,34 +130,42 @@ describe('Testa uso do endpoint /matchs?inProgress', () => {
   let chaiHttpResponse: Response;
 
   describe('Verifica funcionamento do método GET em casos de sucesso', () => {
-    before(async () => {
-      sinon.stub(MatchsServices, "getMatchsByProgress").resolves(responseValidate(200, '', matchsGetMock));
-      sinon.stub(Matchs, "findAll").resolves(matchFindAll as Matchs[]);
+    describe('Verifica o endpoint "/matchs/?inProgress=true"', () => {
+      before(async () => {
+        sinon.stub(Matchs, "findAll").resolves(matchsGetProgressTrue as unknown as Matchs[]);
+      })
+  
+      after((async () => {
+        (Matchs.findAll as sinon.SinonStub).restore();
+      }))
+      it('Retorna os dados esperados ao fazer uma requisição correta utilizando inProgress=true', async () => {
+        chaiHttpResponse = await chai.request(app).get('/matchs/?inProgress=true');
+        expect(chaiHttpResponse.body).to.deep.equal(matchsGetProgressTrue);
+        expect(chaiHttpResponse.status).to.be.equal(200);
+      });
     })
-
-    after((async () => {
-      (MatchsServices.getMatchsByProgress as sinon.SinonStub).restore();
-      (Matchs.findAll as sinon.SinonStub).restore();
-    }))
-    it('Retorna os dados esperados ao fazer uma requisição correta utilizando inProgress=true', async () => {
-      chaiHttpResponse = await chai.request(app).get('/matchs/?inProgress=true');
-      expect(chaiHttpResponse.body).to.deep.equal(matchsGetProgressTrue);
-      expect(chaiHttpResponse.status).to.be.equal(200);
-    });
-    it('Retorna os dados esperados ao fazer uma requisição correta utilizando inProgress=false', async () => {
-      chaiHttpResponse = await chai.request(app).get('/matchs/?inProgress=false');
-      expect(chaiHttpResponse.body).to.deep.equal(matchsGetProgressFalse);
-      expect(chaiHttpResponse.status).to.be.equal(200);
-    });
+    describe('Verifica o endpoint "/matchs/?inProgress=true"', () => {
+      before(async () => {
+        sinon.stub(Matchs, "findAll").resolves(matchsGetProgressFalse as unknown as Matchs[]);
+      })
+  
+      after((async () => {
+        (Matchs.findAll as sinon.SinonStub).restore();
+      }))
+      it('Retorna os dados esperados ao fazer uma requisição correta utilizando inProgress=false', async () => {
+        chaiHttpResponse = await chai.request(app).get('/matchs/?inProgress=false');
+        expect(chaiHttpResponse.body).to.deep.equal(matchsGetProgressFalse);
+        expect(chaiHttpResponse.status).to.be.equal(200);
+      });
+    })
   })
+
   describe('Verifica funcionamento do método GET em casos de erro', () => {
     before(async () => {
-      sinon.stub(MatchsServices, "getMatchsByProgress").resolves(responseValidate(404, 'Could not find any Matchs'));
       sinon.stub(Matchs, "findAll").resolves([]);
     })
 
     after((async () => {
-      (MatchsServices.getMatchsByProgress as sinon.SinonStub).restore();
       (Matchs.findAll as sinon.SinonStub).restore();
     }))
 
