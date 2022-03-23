050e212e58ee06331aa37bbd525f1754fa58c63b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-23 17:52:05 -0300

    feat: adiciona novas mensagens de erro

--- app/backend/src/database/utils/ErrorMessage.ts
+++ app/backend/src/database/utils/ErrorMessage.ts
@@ -2,6 +2,8 @@ enum ErrorMessage {
   NOT_EMPTY = 'All fields must be filled',
   INCORRECT_LOGIN = 'Incorrect email or password',
   INVALID_TOKEN = 'Invalid Token',
+  NO_ID = 'There is no team with such id!',
+  TEAMS_CONFLIT = 'It is not possible to create a match with two equal teams',
 }
 
 export default ErrorMessage;
