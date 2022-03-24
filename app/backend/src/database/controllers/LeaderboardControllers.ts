71b211b16d81f815d2e69880fa462f3469dd3a0b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-24 14:36:01 -0300

    feat: cria controller leaderbord

---
+++ app/backend/src/database/controllers/LeaderboardControllers.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import * as LeaderboardServices from '../services/LeaderboardServices';
+
+export const removeLint = '';
+
+export const getResult = async (_req: Request, res: Response) => {
+  const { status, message, data } = await LeaderboardServices.getResult();
+  if (status >= 400) {
+    return res.status(status).json({ message });
+  }
+  return res.status(200).json(data);
+};
