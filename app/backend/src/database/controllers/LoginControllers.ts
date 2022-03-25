795bd93bae10383dbd26e9ae997ad647d2be4be1
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-25 16:44:29 -0300

    refactor: converte LoginController para classe

--- app/backend/src/database/controllers/LoginControllers.ts
+++ app/backend/src/database/controllers/LoginControllers.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from 'express';
 import * as UserServices from '../services/UserServices';
 
-export const checkLogin = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const { status, message, data } = await UserServices.getUserById({ email, password });
-  if (status >= 400) {
-    return res.status(status).json({ message });
+export default class Login {
+  static async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+    const { status, message, data } = await UserServices.getUser({ email, password });
+    if (status >= 400) {
+      return res.status(status).json({ message });
+    }
+    return res.status(200).json(data);
   }
-  return res.status(200).json(data);
-};
 
-export const validate = async (req: Request, res: Response) => {
-  const authorization = req.headers.authorization || '';
-  const { status, message, data } = await UserServices.getRoleByToken(authorization);
-  if (status >= 400) {
-    return res.status(status).json({ message });
+  static async validate(req: Request, res: Response) {
+    const authorization = req.headers.authorization || '';
+    const { status, message, data } = await UserServices.getRoleByToken(authorization);
+    if (status >= 400) {
+      return res.status(status).json({ message });
+    }
+    return res.status(200).json(data);
   }
-  return res.status(200).json(data);
-};
+}
