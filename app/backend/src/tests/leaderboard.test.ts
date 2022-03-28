cc049b4e7c63fa7f2e93de4e7e23aadc5db7d866
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-28 17:40:56 -0400

    TDD: cria testes de integração para o endpoint leaderboard/away

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -66,3 +66,52 @@ describe('Testa uso do endpoint /leaderboard/home', () => {
     });
   })
 });
+
+describe('Testa uso do endpoint /leaderboard/away', () => {
+
+  let chaiHttpResponse: Response;
+
+  describe('Verifica funcionamento do método GET em casos de sucesso', () => {
+    before(async () => {
+      sinon.stub(Clubs, "findAll").resolves(clubsGetMock as Clubs[]);
+      sinon.stub(Matchs, "findAll").resolves(matchFindAll as Matchs[]);
+    })
+     
+    after(async () => {
+      (Clubs.findAll as sinon.SinonStub).restore();
+      (Matchs.findAll as sinon.SinonStub).restore();
+    })
+    
+    it('Retorna os dados esperados ao fazer uma requisição correta', async () => {
+      chaiHttpResponse = await chai.request(app).get('/leaderboard/away');
+      expect(chaiHttpResponse.body[0]).to.have.property('name')
+      expect(chaiHttpResponse.body[0]).to.have.property('totalPoints')
+      expect(chaiHttpResponse.body[0]).to.have.property('totalVictories')
+      expect(chaiHttpResponse.body[0]).to.have.property('totalDraws')
+      expect(chaiHttpResponse.body[0]).to.have.property('totalLosses')
+      expect(chaiHttpResponse.body[0]).to.have.property('goalsFavor')
+      expect(chaiHttpResponse.body[0]).to.have.property('goalsOwn')
+      expect(chaiHttpResponse.body[0]).to.have.property('goalsBalance')
+      expect(chaiHttpResponse.body[0]).to.have.property('totalGames')
+      expect(chaiHttpResponse.body[0]).to.have.property('efficiency')
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+  })
+  describe('Verifica funcionamento do método GET em casos de sucesso', () => {
+    before(async () => {
+      sinon.stub(Clubs, "findAll").resolves([]);
+      sinon.stub(Matchs, "findAll").resolves([]);
+    })
+     
+    after(async () => {
+      (Clubs.findAll as sinon.SinonStub).restore();
+      (Matchs.findAll as sinon.SinonStub).restore();
+    })
+    
+    it('Retorna os dados esperados ao fazer uma requisição correta', async () => {
+      chaiHttpResponse = await chai.request(app).get('/leaderboard/away');
+      expect(chaiHttpResponse.body).to.deep.equal({ message: 'Clubs not found!' })
+      expect(chaiHttpResponse.status).to.be.equal(404);
+    });
+  })
+});
