ace7162c0b68c53866e5d56445d48ea5a1dee576
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-02 19:03:42 -0300

    refactor: altera forma de iterar nos objetos

--- app/backend/src/database/services/Leaderboard.ts
+++ app/backend/src/database/services/Leaderboard.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import ILeaderboard from '../interfaces/ILeaderboard';
 import Matchs from '../models/Matchs';
 
@@ -10,36 +11,44 @@ type FormatLeaderboard = {
 };
 
 export default class Leaderboard {
-  static getTeamPoints(matchs: Matchs[], firstTeamGoals: TeamGoals, secondTeamGoals: TeamGoals) {
+  static formatTeam() {
+    return {
+      totalPoints: 0,
+      totalVictories: 0,
+      totalDraws: 0,
+      totalLosses: 0,
+      goalsFavor: 0,
+      goalsOwn: 0,
+      goalsBalance: 0,
+    };
+  }
+
+  static getTeamPoints(match: Matchs, firstTeamGoals: TeamGoals, secondTeamGoals: TeamGoals) {
     let totalPoints = 0;
     let totalVictories = 0;
     let totalDraws = 0;
     let totalLosses = 0;
 
-    matchs.forEach((match) => {
-      if (match[firstTeamGoals] > match[secondTeamGoals]) {
-        totalPoints += 3;
-        totalVictories += 1;
-      } else if (match[firstTeamGoals] === match[secondTeamGoals]) {
-        totalDraws += 1;
-        totalPoints += 1;
-      } else {
-        totalLosses += 1;
-      }
-    });
+    if (match[firstTeamGoals] > match[secondTeamGoals]) {
+      totalPoints += 3;
+      totalVictories += 1;
+    } else if (match[firstTeamGoals] === match[secondTeamGoals]) {
+      totalDraws += 1;
+      totalPoints += 1;
+    } else {
+      totalLosses += 1;
+    }
 
     return { totalPoints, totalVictories, totalDraws, totalLosses };
   }
 
-  static getGoalsInfo(matchs: Matchs[], firstTeamGoals: TeamGoals, secondTeamGoals: TeamGoals) {
+  static getGoalsInfo(match: Matchs, firstTeamGoals: TeamGoals, secondTeamGoals: TeamGoals) {
     let goalsFavor = 0;
     let goalsOwn = 0;
     let goalsBalance = 0;
 
-    matchs.forEach((match) => {
-      goalsFavor += match[firstTeamGoals];
-      goalsOwn += match[secondTeamGoals];
-    });
+    goalsFavor += match[firstTeamGoals];
+    goalsOwn += match[secondTeamGoals];
 
     goalsBalance = goalsFavor - goalsOwn;
 
@@ -58,14 +67,35 @@ export default class Leaderboard {
   static async format({
     matchsTeam, name, firstTeamGoals, secondTeamGoals,
   }: FormatLeaderboard): Promise<ILeaderboard> {
-    const teamPoints = this.getTeamPoints(matchsTeam, firstTeamGoals, secondTeamGoals);
+    let totalPoints = 0;
+    let totalVictories = 0;
+    let totalDraws = 0;
+    let totalLosses = 0;
+    let goalsFavor = 0;
+    let goalsOwn = 0;
+    let goalsBalance = 0;
+    matchsTeam.forEach((matchTeam) => {
+      const teamPoints = this.getTeamPoints(matchTeam, firstTeamGoals, secondTeamGoals);
+      totalPoints += teamPoints.totalPoints;
+      totalVictories += teamPoints.totalVictories;
+      totalDraws += teamPoints.totalDraws;
+      totalLosses += teamPoints.totalLosses;
+      const goalsInfo = this.getGoalsInfo(matchTeam, firstTeamGoals, secondTeamGoals);
+      goalsFavor += goalsInfo.goalsFavor;
+      goalsOwn += goalsInfo.goalsOwn;
+      goalsBalance += goalsInfo.goalsBalance;
+    });
     const totalGames = matchsTeam.length;
-    const goalsInfo = this.getGoalsInfo(matchsTeam, firstTeamGoals, secondTeamGoals);
-    const efficiency = ((teamPoints.totalPoints / (totalGames * 3)) * 100).toFixed(2);
+    const efficiency = ((totalPoints / (totalGames * 3)) * 100).toFixed(2);
     return {
       name,
-      ...teamPoints,
-      ...goalsInfo,
+      totalPoints,
+      totalVictories,
+      totalDraws,
+      totalLosses,
+      goalsFavor,
+      goalsOwn,
+      goalsBalance,
       totalGames,
       efficiency: Number(efficiency),
     };
